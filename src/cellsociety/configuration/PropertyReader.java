6e58c1c14059ccda905abcc17166d64489ce9e79
Hayden Lau <hpl5@duke.edu>
2020-10-19 14:36:04 -0400

    tests for PropertyReader Class

--- src/cellsociety/configuration/PropertyReader.java
+++ src/cellsociety/configuration/PropertyReader.java
@@ -1,9 +1,8 @@
 package cellsociety.configuration;
 
-import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.PrintWriter;
 import java.lang.reflect.Constructor;
 import java.net.URISyntaxException;
 import java.nio.file.Path;
@@ -17,10 +16,10 @@ import javafx.util.Pair;
 public class PropertyReader {
 
   private final Pair[] defaultOptionalKeys = new Pair[] {
-      new Pair("probCatch", 0.3),
-      new Pair("winThreshold", 3.0),
-      new Pair("satisfiedThreshold", 0.3),
-      new Pair("thresholdForBirth", 3.0),
+      new Pair("probCatch", "0.3"),
+      new Pair("winThreshold", "3.0"),
+      new Pair("satisfiedThreshold", "0.3"),
+      new Pair("thresholdForBirth", "3.0"),
       new Pair("edgePolicy", "finite"),
       new Pair("neighborPolicy", "Complete"),
       new Pair("initialStatePolicy", "Csv")
@@ -55,7 +54,7 @@ public class PropertyReader {
       }
       for (Pair optionalKey : defaultOptionalKeys) {
         if (properties.getProperty((String) optionalKey.getKey()) == null) {
-          properties.put(optionalKey.getKey(), optionalKey.getValue());
+          properties.setProperty((String) optionalKey.getKey(), (String) optionalKey.getValue());
         }
       }
     } catch (IOException e) {
@@ -75,10 +74,7 @@ public class PropertyReader {
   public void setProperty(String key, String value) {
     properties.setProperty(key, value);
     try {
-      PrintWriter writer =
-          new PrintWriter(
-              new File(this.getClass().getResource("/" + file).getPath()));
-      properties.store(writer, null);
+      properties.store(new FileOutputStream(this.getClass().getResource("/" + file).getPath()),null);
     } catch (IOException e) {
       throw new ConfigurationException(
           String.format(configurationErrorsResourceBundle.getString("errorWritingToPropertiesFile"), key, file));
@@ -120,7 +116,7 @@ public class PropertyReader {
       Constructor<?> gridConstructor = configurationGrid.getConstructor(Path.class, String.class, Map.class);
       grid = (Grid) gridConstructor.newInstance(path, simulationName, optionalKeyMap(simulationName));
     } catch (ClassNotFoundException e) {
-      throw new ConfigurationException(String.format(configurationErrorsResourceBundle.getString("simulationNotSupported"), initialConfiguration));
+      throw new ConfigurationException(String.format(configurationErrorsResourceBundle.getString("simulationNotSupported"), simulationName));
     } catch (Exception e) {
       throw new ConfigurationException(String.format(configurationErrorsResourceBundle.getString("otherSimulationCreationErrors"), e.getMessage()));
     }
